97eedb83f7cbcc423153fbb520c2eed11bc4510e
조성혁 <chosh2001@naver.com>
2023-07-17 01:47:41 +0900

    최신화

--- 23305.cpp
+++ 23305.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	vector<int> current; // 현재 신청한 수업
-	vector<int> change; // 교환을 통해 신청하고 싶은 수업
-
 	int n;
 	cin >> n;
+	vector<int> lec(1000001); // 수업 번호가 곧 인덱스임.
 
+	// 인덱스(수업 번호)에 해당하는 값이 수업을 가지고 있는 학생의 수가 됨.
 	for (int i = 0; i < n; i++) {
-		int k;
-		cin >> k;
-		current.push_back(k);
-	}
-	for (int i = 0; i < n; i++) {
-		int k;
-		cin >> k;
-		change.push_back(k);
+		int a;
+		cin >> a;
+		lec[a]++;
 	}
 
-	// change에 있는 수업이 current에 있으면 원하는 수업을 들을 수 있음.
+	// 교환을 통해 수강할 수업이 들어오면 1을 뺀다. 만약 0이라면 교환 불가.
 	int cnt = 0;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (change[i] == current[j]) {
-				cnt++;
-				current[j] = -1;
-				break;
-			}
-		}
+		int b;
+		cin >> b;
+		if (lec[b] >= 1)
+			lec[b] -= 1;
+		else
+			cnt++;
 	}
 
-	cout << n - cnt;
+	cout << cnt;
 }
