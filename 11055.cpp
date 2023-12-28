cce59bbc4ee6f36d38aa7f2dbae85bb285d377e7
조성혁 <chosh2001@naver.com>
2023-12-28 11:32:46 +0900

    최신화

--- 11055.cpp
+++ 11055.cpp
@@ -14,15 +14,18 @@ int main() {
 		v.push_back(x);
 	}
 
-	vector<int> dp;
+	vector<int> dp; // dp[i]: v[i]까지의 가장 큰 증가하는 수열의 합
 	int max = 0;
 	for (int i = 0;i < n;i++) {
-		int length = 0;
+		int sum = 0;
+		int tmp = 0;
 		for (int j = 0;j < i;j++) {
-			if (v[i] > v[j])
-				length = (length > dp[j]) ? length : dp[j];
+			if (v[i] > v[j]) {
+				tmp = dp[j];
+				sum = (tmp > sum) ? tmp : sum;
+			}
 		}
-		dp.push_back(length + 1);
+		dp.push_back(v[i] + sum);
 		max = (dp[i] > max) ? dp[i] : max;
 	}
 
