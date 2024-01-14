b9b4843200ac647e66737592337ad89dc6d6deab
조성혁 <chosh2001@naver.com>
2024-01-14 12:01:53 +0900

    240114

--- 11051.cpp
+++ 11051.cpp
@@ -2,16 +2,21 @@
 
 using namespace std;
 
+int dp[1001][1001];
+
 int main() {
 	int n, k;
 	cin >> n >> k;
-	int x = 1;
-	for (int i = 0;i < k;i++) {
-		x *= n - i;
-		x %= 10007;
+
+	for (int i = 0;i <= n;i++) {
+		for (int j = 0;j <= i;j++) {
+			if (j == 0 || j == n)
+				dp[i][j] = 1;
+			else
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+		}
 	}
-	for (int i = 0;i < k;i++) {
-		x /= k - i;
-	}
-	cout << x;
+
+	cout << dp[n][k];
+	
 }
