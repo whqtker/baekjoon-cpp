8f1ea98dfaaf79ce63c0515c93a684004d91ca1d
조성혁 <chosh2001@naver.com>
2024-03-31 17:04:54 +0900

    240331

--- 1807.cpp
+++ 1807.cpp
@@ -30,32 +30,26 @@ int main() {
 
 			// A[N]에서 N이 4의 배수면 A[N]의 길이는 N의 길이와 동일하다.
 			// N이 4의 배수가 아니면 A[N]의 길이는 N의 길이 + 1이다.
-			ll N = 1; // m에 존재하는 수(A[N])의 N의 값
-			ll s = 0; // m에 존재하는 수의 첫 번째 숫자의 인덱스
-			while (s + len(N) < m) {
-				cout << s << "가 시작인덱스인 N: " << N << "\n";
-				if (N % 4 == 0) {
-					s += len(N);
-				}
-				else {
-					s += len(N) + 1;
-				}
-				N++;
-			}
-			ll e = N % 4 == 0 ? s + len(N) : s + len(N) - 1; // m에 존재하는 수의 마지막 숫자의 인덱스
+			ll s = 0; // m에 존재하는 수 A[N]의 첫 번째 숫자의 인덱스
+			
+			ll digit = len(m);
+
+			ll e = m % 4 == 0 ? s + len(m)-1 : s + len(m); // m에 존재하는 수의 마지막 숫자의 인덱스
 
 			if (s <= n && n <= e) {
-				if (N % 4 != 0 && n == e) {
+				if (m % 4 != 0 && n == e) {
 					// N의 일의 자리수 * 10 + x가 4의 배수가 되는 x 찾기
-					int num = N % (int)pow(10, len(N));
+					int num = m % (int)pow(10, len(m));
 					for (int i = 0;i < 10;i++) {
 						if ((num + i) % 4 == 0) {
 							cout << i << "\n";
+							break;
 						}
 					}
 				}
 				else {
-					cout << to_string(N)[n - s] << "\n";
+					cout << to_string(m)[n - s] << "\n";
+					break;
 				}
 			}
 			else if (n < s) {
