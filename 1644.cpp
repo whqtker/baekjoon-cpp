ac56dcb674ad92fe6ac65a1b95aea3f071a12e54
조성혁 <chosh2001@naver.com>
2024-04-04 16:39:47 +0900

    240404

--- 1644.cpp
+++ 1644.cpp
@@ -11,8 +11,8 @@ int main() {
 
 	for (int i = 2; i * i <= n; i++) {
 		if (!arr[i]) {
-			for (int j = i * i; j <= n; j++) {
-				arr[i] = 1;
+			for (int j = i * i; j <= n; j += i) {
+				arr[j] = 1;
 			}
 		}
 	}
@@ -23,8 +23,29 @@ int main() {
 			v.push_back(i);
 	}
 
-	int l = 2, r = 2;
-	while (1) {
-
+	if (n == 1) {
+		cout << 0;
+		return 0;
 	}
+
+	int l = 0, r = 0, sum = v[0], cnt = 0;
+	while (1) {
+		if (sum < n) {
+			r++;
+			if (r >= v.size())
+				break;
+			sum += v[r];
+		}
+		else if (sum > n) {
+			sum -= v[l];
+			l++;
+		}
+		else {
+			cnt++;
+			sum -= v[l];
+			l++;
+		}
+	}
+
+	cout << cnt;
 }
