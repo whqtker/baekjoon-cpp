3b8040652ebb22239ed1fc7082102abb65fe4945
조성혁 <chosh2001@naver.com>
2024-05-08 17:03:29 +0900

    240508

--- 31413.cpp
+++ 31413.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int v[1000]; // 봉사활동 가산점
-int dp[1000][1000]; // dp[i][j]: i일까지의 최대 가산점, 헌혈 j번 함
+int dp[2001][1000]; // dp[i][j]: i일까지의 최대 가산점, 헌혈 j번 함
 
 int main() {
 	int n, m, a, d; // n: 남은 일수, m: 필요한 가산점, a: 헌혈 가산점, d: 헌혈 후 쉬어야 하는 날
@@ -15,30 +15,29 @@ int main() {
 	cin >> a >> d;
 
 	dp[0][0] = v[0];
-	dp[0][1] = a;
+	dp[d - 1][1] = a;
 
 	int ans = 1001;
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j <= i; j++) {
+	for (int i = 1;i < n;i++) {
+		for (int j = 0;j <= i;j++) {
 			// 봉사활동을 하는 경우
-			if (dp[i - 1][j] > dp[i - 1][j - 1]) {
-				dp[i][j] = dp[i - 1][j] + v[i];
-			}
-			// 헌혈을 하는 경우
-			else {
-				dp[i][j] = dp[i - 1][j - 1] + a;
-				for (int k = 1; k <= d; k++) {
-					dp[i + k][j] = dp[i][j];
-				}
-			}
-
+			// 전날 최대 가산점이 존재하지 않는 경우
+			if (dp[i - 1][j] == 0)
+				continue;
+			dp[i][j] = max(dp[i][j], dp[i - 1][j] + v[i]);
 			if (dp[i][j] >= m)
 				ans = min(ans, j);
+
+			// 헌혈을 하는 경우, 헌혈 후 쉬는 날 고려, 쉬는 날은 dp 초기화 X
+			dp[i + d - 1][j + 1] = max(dp[i + d - 1][j + 1], dp[i - 1][j] + a);
+			if (dp[i + d - 1][j + 1] >= m)
+				ans = min(ans, j + 1);
+
 		}
 	}
 
-	if (ans != 1001)
-		cout << ans;
-	else
+	if (ans == 1001)
 		cout << -1;
+	else
+		cout << ans;
 }
