a05b36d67cf504d59ee0d65a520c94faeee3922d
seonghyeok <chosh2001@naver.com>
2025-04-27 10:26:34 +0900

    250427

--- solutions/1208.cpp
+++ solutions/1208.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
+int arr[41];
 int n, s;
+map<int, int> m;
 long long ans;
-int arr[40];
-int visited[40];
 
-// 배열에 접근하는 인덱스는 y보다 같거나 커야 함
-void dfs(int x, int y) {
-	if (x > n) {
+// 왼쪽 부분수열의 합
+void left(int idx, int sum) {
+	if (idx == n / 2) {
+		m[sum]++;
 		return;
 	}
 
-	if (x != 0) {
-		int sum = 0;
-		for (int i = 0;i < n;i++) {
-			if (visited[i])
-				sum += arr[i];
-		}
-		if (sum == s) {
-			ans++;
-		}
+	left(idx + 1, sum);
+	left(idx + 1, sum + arr[idx]);
+}
+
+// 오른쪽 부분수열의 합
+void right(int idx, int sum) {
+	if (idx == n) {
+		ans += m[s - sum];
+		return;
 	}
 
-	for (int i = y;i < n;i++) {
-		if (!visited[i]) {
-			visited[i] = 1;
-			dfs(x + 1, i + 1);
-			visited[i] = 0;
-		}
-	}
+	right(idx + 1, sum);
+	right(idx + 1, sum + arr[idx]);
 }
 
 int main() {
@@ -38,10 +36,16 @@ int main() {
 	ios::sync_with_stdio(0);
 
 	cin >> n >> s;
+
 	for (int i = 0;i < n;i++) {
 		cin >> arr[i];
 	}
 
-	dfs(0, 0);
+	left(0, 0);
+	right(n / 2, 0);
+
+	if (s == 0)
+		ans--;
+
 	cout << ans;
 }
